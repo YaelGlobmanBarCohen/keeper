055129556a335f6f3f4e15927b8ef4420bb955db
Yael Globman <yaelglobman@gmail.com>
2024-01-09 16:30:47 +0200

    header, footer and main note

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App(){
+    return (
+        <div>
+        <body>
+            <Header></Header>
+            <Note></Note>
+            <Footer></Footer>
+        </body>
+        </div>
+    );
+}
+
+export default App;
